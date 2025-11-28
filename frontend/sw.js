dfb8ea08c2b3ab333d114a68058acbdd0ccfea84
Sylvain Picker <sylvain.picker@gmail.com>
2025-11-28 01:28:49 -0500

    Create sw.js

---
+++ frontend/sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'seedballs-v2.0';
+const STATIC_ASSETS = ['/', '/search.html', '/contribute/recette.html', '/manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
